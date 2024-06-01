59d9428de21735f3327479760b7a4d98c73435f2
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-06-01 09:08:58 -0400

    fix: :lipstick: make buttons better

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -16,10 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
         slotMaxTime: '23:00:00',
         firstDay: 3,
         headerToolbar: {
-            left: 'prev,next today',
+            left: 'today prev,next',
             center: 'title',
             right: 'timeGridWeek,timeGridDay,listWeek'
         },
+        nowIndicator: true,
         googleCalendarApiKey: 'AIzaSyBhK1Z3vN8QWKNdJX5V4c9SWqMn7jyytO4',
         events: {
             googleCalendarId: '3240885f8cde993cfb4753a3d5b563b5a20bc8936448b5291b67fc6396b70659@group.calendar.google.com'
